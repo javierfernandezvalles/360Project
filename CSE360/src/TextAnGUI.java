4e5c9c520b44ef92aa866926dda69ac33656f036
agonza36 <31782958+agonza36@users.noreply.github.com>
2017-11-13 16:06:24 -0700

    Add files via upload

--- CSE360/src/TextAnGUI.java
+++ CSE360/src/TextAnGUI.java
@@ -1,10 +1,18 @@
 import java.awt.EventQueue;
 
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.LinkedList;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import java.awt.Color;
@@ -17,6 +25,17 @@ import java.awt.SystemColor;
 import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+import org.apache.commons.io.FileUtils;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.UIManager;
 
 public class TextAnGUI {
 
@@ -25,7 +44,7 @@ public class TextAnGUI {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -56,44 +75,97 @@ public class TextAnGUI {
 		frmTextFileAnalyzer.getContentPane().setLayout(null);
 		
 		JButton btnHelp = new JButton("Help?");
+		btnHelp.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		btnHelp.setToolTipText("");
 		btnHelp.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(null, 
-						"1. Please select the text file that you wish to analyze. \n"
-						+ "2. Select Read File, this will give you a break down of the selecteds file.\n"
-						+ "The History Of All Files will provide a list of all the Text files analyzed. \n"
-						+ "The Averages Of All Files will provide statistics of all files analyzed. ");
+						"Please select the text file that you wish to analyze. \n"
+						+ "File History will provide a list of all the Text files analyzed. \n"
+						+ "File Averages will provide statistics of all files analyzed. ");
 			}
+		
 		});
-		btnHelp.setBounds(848, 16, 115, 29);
+		btnHelp.setBounds(631, 389, 205, 40);
 		frmTextFileAnalyzer.getContentPane().add(btnHelp);
 		
+		// use a linked list to store the data from every file processed
+		LinkedList<FileData> history = new LinkedList<FileData>();
 		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setDialogTitle("");
+		fileChooser.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		fileChooser.setApproveButtonText("Open");
+		fileChooser.setFont(UIManager.getFont("EditorPane.font"));
 		FileFilter filter = new FileNameExtensionFilter("Text","txt"); //Filter only allows Text files to be selected
 		fileChooser.setFileFilter(filter);
 		fileChooser.setMultiSelectionEnabled(true);
-		fileChooser.addActionListener(new ActionListener() {
+		fileChooser.addActionListener(new ActionListener() 
+		
+			{
 			public void actionPerformed(ActionEvent e) {
+				// Calendar and DateFormat classes used for recording the time each
+				// file was processed in the program. 
+				Calendar cal;
+				DateFormat fileDate = new SimpleDateFormat("MM/dd/yy HH:mm:ss");
+				String fileName = fileChooser.getSelectedFile().getName();
+				File h = fileChooser.getSelectedFile();
+				if(h.exists()) 
+				{
+						try {
+							String fileContents = FileUtils.readFileToString(h);
+							// make a LL of wordnodes of the fileContents that stores
+							// every word in the file and the frequency at which it 
+							// occurs in the file.
+							// ***MUST DO THIS BEFORE THE OTHER FILE METRICS CAN BE USED***
+							LinkedList<WordNode> L = FileMetrics.wordCount(fileContents);
+							
+							// get date of file processing, store in FileData node for the
+							// history linked list
+							cal = Calendar.getInstance();
+							FileData newFileData = new FileData(fileName, fileDate.format(cal.getTime()));
+							
+							// Use the Linked List L and the String File Contents to calculate and store all the
+							// file metrics in a FileData node
+							newFileData.setAverageCharsPerLine((int) FileMetrics.averageCharsPerLine(L, fileContents));
+							newFileData.setAverageWordLength((int) FileMetrics.averageWordLength(L));
+							newFileData.setBlankLines(FileMetrics.totalEmptyLines(fileContents));
+							newFileData.setLines(FileMetrics.totalLines(fileContents));
+							newFileData.setMostCommon(FileMetrics.topWord(L));
+							newFileData.setSpaces(FileMetrics.totalSpaces(fileContents));
+							newFileData.setWords(FileMetrics.totalWords(L));
+					        JOptionPane.showMessageDialog(null, newFileData.toString());
+							// keep the data from the file stored in the history linked list
+							// by storing a FileData data node in the history linked list
+							history.add(newFileData);
+						} 
+						catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						} 					
+				}	
+				else {
+			        JOptionPane.showMessageDialog(null, "Error: File Doesn't Exist");
+				}
 			}
 		});
-		fileChooser.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		fileChooser.setBounds(37, 16, 796, 334);
+		fileChooser.setBounds(90, 16, 796, 334);
 		frmTextFileAnalyzer.getContentPane().add(fileChooser);
-		
-		JButton btnReadFile = new JButton("Read File");
-		btnReadFile.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		btnReadFile.setBounds(32, 388, 205, 40);
-		frmTextFileAnalyzer.getContentPane().add(btnReadFile);
-		
-		JButton btnHistory = new JButton("History Of All Files");
+			
+		JButton btnHistory = new JButton("File History");// File History JButton
+		btnHistory.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (history.size() == 0) {
+			        JOptionPane.showMessageDialog(null, "No files have been processed.");
+				} else {
+					for (int i = 0; i < history.size(); i++) {
+				        JOptionPane.showMessageDialog(null, history.get(i).getFileName() + " | " + history.get(i).getDate());
+					}
+				}
+				
+			}
+		});
 		btnHistory.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		btnHistory.setBounds(394, 388, 205, 40);
+		btnHistory.setBounds(143, 389, 205, 40);
 		frmTextFileAnalyzer.getContentPane().add(btnHistory);
-		
-		JButton btnAverages = new JButton("Averages Of All Files");
-		btnAverages.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		btnAverages.setBounds(744, 388, 205, 40);
-		frmTextFileAnalyzer.getContentPane().add(btnAverages);
 	}
 }
