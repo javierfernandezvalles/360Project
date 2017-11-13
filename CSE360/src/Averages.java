32f19c3adeedc4bbb27fdff94787e38679690b2b
Codyrg <32079255+Codyrg@users.noreply.github.com>
2017-11-13 16:22:06 -0700

    finished writing the Averages class
    computes averages of metrics across all files processed

--- CSE360/src/Averages.java
+++ CSE360/src/Averages.java
@@ -1,11 +1,43 @@
+import java.util.LinkedList;
 
 // Used for computation of average metrics across all files
 public class Averages {
-	int avgLines, avgBlankLines, avgSpaces,
-	avgWords, avgAvgCharsPerLine, avgAvgWordLength;
+	int avgLines = 0, avgBlankLines = 0, avgSpaces = 0,
+	avgWords = 0, avgAvgCharsPerLine = 0, avgAvgWordLength = 0;
 	
 	
-	/*public Averages(LinkedList<FileData> history){
-		
-	}*/
+	public Averages() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public void compute(LinkedList<FileData> history){
+		int s = history.size();
+		int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
+		for(int i = 0; i < s; i++){
+			a += history.get(i).getLines();
+			b += history.get(i).getBlankLines();
+			c += history.get(i).getSpaces();
+			d += history.get(i).getWords();
+			e += history.get(i).getAverageCharsPerLine();
+			f += history.get(i).getAverageWordLength();
+
+		}
+		if(s != 0){
+			avgLines = a/s;
+			avgBlankLines = b/s;
+			avgSpaces = c/s;
+			avgWords = d/s;
+			avgAvgCharsPerLine = e/s;
+			avgAvgWordLength = f/s;
+		}
+	}
+
+
+	@Override
+	public String toString() {
+		return "Averages [avgLines=" + avgLines + ", avgBlankLines=" + avgBlankLines + ", avgSpaces=" + avgSpaces
+				+ ", avgWords=" + avgWords + ", avgAvgCharsPerLine=" + avgAvgCharsPerLine + ", avgAvgWordLength="
+				+ avgAvgWordLength + "]";
+	}
 }
