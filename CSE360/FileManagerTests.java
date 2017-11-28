36fbb96fbc5d49fd9acbb4c57ee5645481968fc4
jferna29 <31553526+jferna29@users.noreply.github.com>
2017-11-27 17:02:12 -0700

    Modified tests + added tests/files

--- CSE360/FileManagerTests.java
+++ CSE360/FileManagerTests.java
@@ -27,14 +27,14 @@ public class FileManagerTests {
 	public void testCorrectNumberOfSpaces() {
 		String fileName = "TestFile02.txt";
 		String fileContents = FileUtils.readFileToString(fileName);
-		assertEquals(7, FileMetrics.totalSpaces(fileContents));
+		assertEquals(6, FileMetrics.totalSpaces(fileContents));
 	}
 	
 	@Test
 	public void testCorrectNumberOfWords() {
 		String fileName = "TestFile03.txt";
 		String fileContents = FileUtils.readFileToString(fileName);
-		assertEquals(6, FileMetrics.totalSpaces(fileContents));
+		assertEquals(7, FileMetrics.totalSpaces(fileContents));
 	}
 	
 	@Test
@@ -42,6 +42,22 @@ public class FileManagerTests {
 		String fileName = "TestFile04.txt";
 		String fileContents = FileUtils.readFileToString(fileName);
 		LinkedList<WordNode> L = FileMetrics.wordCount(fileContents);
-		assertEquals(6, (int)FileMetrics.averageCharsPerLine(L, fileContents));
+		assertEquals(12, (int)FileMetrics.averageCharsPerLine(L, fileContents));
+	}
+	
+	@Test
+	public void testAverageWordLength() {
+		String fileName = "TestFile05.txt";
+		String fileContents = FileUtils.readFileToString(fileName);
+		LinkedList<WordNode> L = FileMetrics.wordCount(fileContents);
+		assertEquals(3.7, FileMetrics.averageWordLength(L));
+	}
+	
+	@Test
+	public void testMostCommonWords() {
+		String fileName = "TestFile06.txt";
+		String fileContents = FileUtils.readFileToString(fileName);
+		LinkedList<WordNode> L = FileMetrics.wordCount(fileContents);
+		assertEquals("Cloud", FileMetrics.topWord(L));
 	}
 }
